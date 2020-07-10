a83e9d7d300cbaceef8f10f62de4b3b017bceb75
igor-morawski <igor.morawski@hotmail.com>
2020-07-10 10:13:40 +0800

    devices by mac

--- htpaisc/communication.py
+++ htpaisc/communication.py
@@ -8,6 +8,8 @@ import select
 
 import htpaisc.utils
 
+VERBOSE = False
+
 SOCK_TIMEOUT = 1
 
 HTPA_PORT = 30444
@@ -81,8 +83,8 @@ class Device:
     address : tuple
         Device's (ip, port) in an ordered tuple
     """
-    def __init__(self, ip, port=None, verbose=False):
-        self.verbose = verbose
+    def __init__(self, mac, ip, port=None):
+        self.mac = mac
         self.ip = ip
         if not port:
             self.port = HTPA_PORT
@@ -94,23 +96,23 @@ class Device:
         self.sock.settimeout(SOCK_TIMEOUT)
 
     def connect(self):
-        htpaisc.utils.print_if_verbose("Connecting to {}...".format(self.ip), self.verbose)
+        htpaisc.utils.print_if_verbose("Connecting to {}, MAC-ID: {}...".format(self.ip, self.mac), VERBOSE)
         # Bind
         try:
             self.sock.sendto(HTPA_BIND_MSG.encode(), self.address)
             _ = self.sock.recv(BUFF_SIZE)
             return True
         except socket.timeout:
-            htpaisc.utils.print_if_verbose("Could not bind device {}".format(self.ip), self.verbose)
+            htpaisc.utils.print_if_verbose("Could not bind device {}, MAC-ID: {}...".format(self.ip, self.mac), VERBOSE)
             return False
     
     def release(self):
         try:
-            htpaisc.utils.print_if_verbose("Released device {}".format(self.ip), self.verbose)
             self.sock.sendto(HTPA_RELEASE_MSG.encode(), self.address)
+            htpaisc.utils.print_if_verbose("Released device {}, MAC-ID: {}".format(self.ip, self.mac), VERBOSE)
             return True
         except socket.timeout:
-            htpaisc.utils.print_if_verbose("Could not release device {}".format(self.ip), self.verbose)
+            htpaisc.utils.print_if_verbose("Could not release device {}, MAC-ID: {}".format(self.ip, self.mac), VERBOSE)
             return False
 
 
@@ -134,26 +136,30 @@ class Device_Manager:
     HTPA32x32d device manager
     # TODO: Docs
     """
-    def __init__(self, log_dir, bcast_addr, verbose=False):
-        self.verbose = verbose
+    def __init__(self, log_dir):
         if not os.path.exists(log_dir):
             os.mkdir(log_dir)
         if not os.access(log_dir, os.W_OK):
             raise Exception("Specified directory is not writeable.")
-        self._bcast_addr = bcast_addr
-        if not validateIP(self._bcast_addr):
-            raise ValueError("Specified broadcasting address ({}) is incorrect.".format(self._bcast_addr))
         self._broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._broadcast_socket.bind(('', 0))
         self._broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self._device_addresses_l = []
+        self._device_info_l = []
         self._connected_devices = []
 
-    def _add_device_address(self, addr):
-        if addr not in self._device_addresses_l:
-            self._device_addresses_l.append(addr)
+    def _add_device_info(self, info):
+        """
+        # XXX
+        Parameters
+        ----------
+        info : dictionary
+            Info about an HTPA32x32d device: ip, mac, port, etc.
+            Use lower-case in key-words.
+        """
+        if info not in self._device_info_l:
+            self._device_info_l.append(info)
 
-    def scan(self, duration = 1., timeout = 0.1):
+    def scan(self, bcast_addr='<broadcast>',  duration = 1., timeout = 0.1):
         """
         Scans for HTPA32x32d devices and adds newly discovered devices to the Device Manager. 
         Only 32x32 devices are discovered, other resolutions are ignored.
@@ -168,36 +174,43 @@ class Device_Manager:
         bool
             True if the scan is finished
         """
-        if self.verbose: 
-            print("Scanning network...")
-        self._broadcast_socket.sendto(HTPA_CALLING_MSG.encode(), (self._bcast_addr, HTPA_PORT))
+        htpaisc.utils.print_if_verbose("Scanning network...", VERBOSE)
+        self._broadcast_socket.sendto(HTPA_CALLING_MSG.encode(), (bcast_addr, HTPA_PORT))
+
+        if not validateIP(bcast_addr):
+            raise ValueError("Specified broadcasting address ({}) is incorrect.".format(bcast_addr))
         
+        d_discovered = False
         iteration = 0
         while iteration < duration / timeout:
             ready = select.select([self._broadcast_socket], [], [], timeout)
             if ready[0]:
                 data, addr = self._broadcast_socket.recvfrom(BUFF_SIZE)
                 if HTPA32x32d_IDENT_MSG_CHUNK in data.decode():
-                    htpaisc.utils.print_if_verbose("HTPA32x32d device discovered at {}".format(addr[0]), self.verbose)
-                    self._add_device_address(addr)
+                    mac = str(data).split("MAC-ID: ")[1].split(" ")[0]
+                    htpaisc.utils.print_if_verbose("HTPA32x32d device discovered at {}, MAC-ID: {}".format(addr[0], mac), VERBOSE)
+                    self._add_device_info({"mac" : mac, "ip" : addr[0], "port" : addr[1]})
+                    d_discovered = True
             iteration += 1
-        return True
+        if d_discovered:
+            return True
+        else:
+            htpaisc.utils.print_if_verbose("No devices discovered", VERBOSE)
     
     def _add_connected_device(self, device_obj):
         if device_obj not in self._connected_devices:
             self._connected_devices.append(device_obj)
-            htpaisc.utils.print_if_verbose("Added succsesfuly connected device {}".format(device_obj.ip), self.verbose)
+            htpaisc.utils.print_if_verbose("Added a succsesfuly connected device {}, MAC-ID: {}".format(device_obj.ip, device_obj.mac), VERBOSE)
 
     def connect(self):
         """
         Connects to all devices discovered when scanning.
         # TODO
         """
-        if not len(self._device_addresses_l):
+        if not len(self._device_info_l):
             raise ValueError("No devices added to the manager. Discover devices by calling 'scan' method.")
-        for addr in self._device_addresses_l:
-            ip, port =  addr
-            d = Device(ip, port, verbose=self.verbose)
+        for info in self._device_info_l:
+            d = Device(**info)
             if d.connect():
                 self._add_connected_device(d)
     
@@ -206,8 +219,9 @@ class Device_Manager:
         Releases binded HTPA32x32d devices. 
         # TODO
         """
-        for d in self._connected_devices:
+        for d in self._connected_devices.copy():
             d.release()
+            self._connected_devices.remove(d)
 
     def caputre_voltage_frames(self):
         """
@@ -223,5 +237,15 @@ class Device_Manager:
             if frame:
                 result.append(frame)
         return result
+
+    def get_device_by_mac(self, mac):
+        """
+        # TODO 
+        """
+        for d in self._connected_devices:
+            if d.mac == mac:
+                return d
+        htpaisc.utils.print_if_verbose("Device with MAC-ID {} not connected".format(mac))
+        return None
 
 
--- main.py
+++ main.py
@@ -2,23 +2,46 @@ import argparse
 import os
 import htpaisc
 
+DEBUG = True
+
+# XXX missing frames?
+
+
+def logging(args):
+    htpaisc.communication.VERBOSE = args.verbose
+    device_manager = htpaisc.communication.Device_Manager(args.log_dir)
+    if DEBUG:
+        if device_manager.scan(bcast_addr="140.123.112.255"):
+            device_manager.connect()
+            frames = device_manager.caputre_voltage_frames()
+            # XXX logging
+            device_manager.release()
+            return frames
+        else:
+            return None
+    else:
+        raise NotImplementedError
+
+def calibration(args):
+    htpaisc.communication.VERBOSE = args.verbose
+    device_manager = htpaisc.communication.Device_Manager(args.log_dir)
+    pass
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='HTPA for ISelfCheck')
-    parser.add_argument('log_dir', nargs='?', default="logs")
+    parser.add_argument('--log', '-l', dest='log', action="store_true")
+    parser.add_argument('--log_dir', dest='log_dir', default="logs")
     parser.add_argument('--verbose', '-v', dest='verbose', action="store_true")
     parser.add_argument('--display', '-d', dest='display', action="store_true")
+    parser.add_argument('--calibrate', '-c', dest='calibrate', action="store_true")
+    parser.add_argument('--calib_dir', dest='calib_dir', default="calib")
     args = parser.parse_args()
 
     if args.display:
         raise NotImplementedError("Not implemented yet")
     
-    device_manager = htpaisc.communication.Device_Manager(args.log_dir, bcast_addr="140.123.112.255", verbose=args.verbose)
-    device_manager.scan()
-    device_manager.connect()
-
-    frames = device_manager.caputre_voltage_frames()
-    if len(frames):
-        import pickle
-        with open("frames.pkl", "wb") as f:
-            pickle.dump(frames, f)
-    device_manager.release()
+    if args.calibrate:
+        calibration(args)
+    elif args.log:
+        logging(args)
+    
